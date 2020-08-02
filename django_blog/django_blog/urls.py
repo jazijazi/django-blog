197694f5a75bc0b3a507c164459b974acef02f66
jazi <jazi1374@gmail.com>
2020-08-02 17:21:53 +0430

    add emain confirmation for signup

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
@@ -25,7 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #path('blog/' , include('blog.urls')),
     path('' , include('blog.urls')),
-    path('register/' , user_views.register , name='register'),
+    #path('register/' , user_views.register , name='register'),
+    path('register/', user_views.Register.as_view(), name='register'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        user_views.activate, name='activate'),
     path('profile/' , user_views.profile , name='profile'),
     path('login/' , auth_views.LoginView.as_view(template_name='users/login.html') , name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
